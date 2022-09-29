3105a99e9daabf8a22e68e8dde1991e1d8634c58
Fazliddin <example@gmail.com>
2022-09-29 11:54:50 +0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnr0VbqA-xOot9ULAlF9esfH7qVm-Y7z0",
+//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "chat-3b9e9.firebaseapp.com",
+  projectId: "chat-3b9e9",
+  storageBucket: "chat-3b9e9.appspot.com",
+  messagingSenderId: "138246423130",
+  appId: "1:138246423130:web:41a9c21298f05535ff4e63"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthContextProvider } from "./context/AuthContext";
+import { ChatContextProvider } from "./context/ChatContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <AuthContextProvider>
+    <ChatContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChatContextProvider>
+  </AuthContextProvider>
+); 
